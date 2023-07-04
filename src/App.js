c9a92c020848cb27e328f71c72d6f730af992716
GrayHead <ss.zhuravlov@gmail.com>
2023-07-04 21:41:25 +0300

    hw1 part2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ const App = () => {
             <Character id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"}
                        image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
 
+            <Character id={1} name={"Rick Sanchez"} status={"Alive"} species={"Human"} gender={"Male"}
+                       image={"https://rickandmortyapi.com/api/character/avatar/1.jpeg"}/>
+
 
         </div>
     );
