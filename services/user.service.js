e1f16867513e7bbd60ecb723bca3b434b7db380f
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2023-08-16 15:15:09 +0300

    TS base

--- services/user.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userService = void 0;
-var axios_1 = require("axios");
-var userService = {
-    getAll: function () { return axios_1.default.get('https://jsonplaceholder.typicode.com/users'); },
-    getById: function (id) { return axios_1.default.get('https://jsonplaceholder.typicode.com/users/' + id); },
-};
-exports.userService = userService;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import {FC} from "react";
+
+const App: FC = () => {
+    return (
+        <div>
+            App
+        </div>
+    );
+};
+
+export {App};
